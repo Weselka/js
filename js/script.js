177a4bdd9cad094083691a2b8fb81ea658a3a611
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-08-04 18:15:34 +0300

    exercize one

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// №1
+// Оголоси дві змінні, productName для назви товару, і pricePerItem для зберігання ціни за штуку. При оголошенні надай змінним наступні значення:
+// назва - рядок "Droid";
+// ціна за штуку - число 2000.
+
+// const productName = "Droid";
+// const pricePerItem = 2000;
+// console.log(productName);
+// console.log(typeof productName);
+// console.log(pricePerItem);
+// console.log(typeof pricePerItem);
+
+// №2
+// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів. Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+let productName = "Droid";
+console.log(productName);
+// let pricePerItem = 2000;
