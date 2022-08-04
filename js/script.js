b2200e503a0ba3d082bc5b5e75e9020a86131e13
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-08-04 18:47:41 +0300

    exercise four

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 // №1
-// Оголоси дві змінні, productName для назви товару, і pricePerItem для зберігання ціни за штуку. При оголошенні надай змінним наступні значення:
-// назва - рядок "Droid";
-// ціна за штуку - число 2000.
-
 // const productName = "Droid";
 // const pricePerItem = 2000;
 // console.log(productName);
@@ -11,22 +7,19 @@
 // console.log(typeof pricePerItem);
 
 // №2
-// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів. Перевизнач значення змінних pricePerItem і productName після їх оголошення.
-
 // productName = "Repair droid";
 // pricePerItem += 1500;
 
 // №3
-// Оголоси наступні змінні, використовуючи ключове слово const або let, і присвой їм відповідні значення.
-
-// topSpeed - число 160.
-// distance - число 617.54.
-// login - рядок "mango935".
-// isOnline - буль true.
-// isAdmin - буль false.
-
 // const topSpeed = 160;
 // let distance = 617.54;
 // let login = "mango935";
 // const isOnline = true;
 // const isAdmin = false;
+
+// №4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// №5
