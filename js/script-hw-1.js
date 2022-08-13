9685686aee77fcf6e34ce3f1805b882c2ae6e055
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-08-13 11:27:09 +0300

    +

--- js/script-hw-1.js
+++ js/script-hw-1.js
@@ -179,8 +179,35 @@
 // }
 
 // №34
-function normalizeInput(input) {
-  const normalizedInput = ; // Change this line
+// function normalizeInput(input) {
+//   const normalizedInput = ; // Change this line
 
-  return normalizedInput;
-}
+//   return normalizedInput;
+// }
+
+// №2.9
+// function getExtremeElements(array) {
+//   // Change code below this line
+//   console.log(array);
+//   console.log(getExtremeElements.length);
+//   const getExtremeElements =
+//   // getExtremeElements.splice(',');
+//   // console.log(array);
+//   // getExtremeElements.slice(1, - 1);
+//   //getExtremeElements.slice(1, -1);
+//   // return getExtremeElements;
+//   // Change code above this line
+// }
+// getExtremeElements([1, 2, 3, 4, 5])
+// повертає[1, 5]
+// getExtremeElements(["Earth", "Mars", "Venus"])
+// повертає["Earth", "Venus"]
+// getExtremeElements(["apple", "peach", "pear", "banana"])
+// повертає["apple", "banana"]
+
+
+// Записать массив, const arr = [“BEST”, “the”, “foo”, “is”,  “JS” ]
+// развернуть массив,
+// вырезать foo,
+// перевести его в строку разделенную пробелами
+;
---
+++ js/script-hw-2.js
@@ -0,0 +1,23 @@
+// №2.9
+// function getExtremeElements(array) {
+//   // Change code below this line
+//   console.log(array);
+//   console.log(getExtremeElements.length);
+//   const getExtremeElements =
+//   // getExtremeElements.splice(',');
+//   // console.log(array);
+//   // getExtremeElements.slice(1, - 1);
+//   //getExtremeElements.slice(1, -1);
+//   // return getExtremeElements;
+//   // Change code above this line
+// }
+// getExtremeElements([1, 2, 3, 4, 5])
+// повертає[1, 5]
+// getExtremeElements(["Earth", "Mars", "Venus"])
+// повертає["Earth", "Venus"]
+// getExtremeElements(["apple", "peach", "pear", "banana"])
+// повертає["apple", "banana"]
+// Записать массив, const arr = [“BEST”, “the”, “foo”, “is”,  “JS” ]
+// развернуть массив,
+// вырезать foo,
+// перевести его в строку разделенную пробелами
