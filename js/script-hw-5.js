63aca450a0e02f837d9a0e956ceff16cdb767256
Weselka <weselyna.shvets@gmail.com>
2022-09-04 20:17:29 +0300

    2022-09-04

--- js/script-hw-5.js
+++ js/script-hw-5.js
@@ -74,18 +74,472 @@
 // };
 
 //4
-const parent = {
-  name: 'Stacey',
-  surname: 'Moore',
-  age: 54,
-  heritage: 'Irish',
-};
-// Change code below this line
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish',
+// };
+// // Change code below this line
 
-const child = {};
+// const child = Object.create(parent);
+// console.log(child);
+// // Change code above this line
+// child.name = 'Jason';
+// child.age = 27;
+// console.log(child);
 
-// Change code above this line
-child.name = 'Jason';
-child.age = 27;
+// console.log(parent.hasOwnProperty('surname'));  // повертає true
+// console.log(parent.hasOwnProperty('heritage')); // // повертає true
+// console.log(child.hasOwnProperty('name')); // // повертає true
+// console.log(child.name); // // повертає "Jason"
+// console.log(child.hasOwnProperty('age')); // // повертає true
+// console.log(child.age); // // повертає 27
+// console.log(child.hasOwnProperty('surname')); // // повертає false
+// console.log(child.surname); // // повертає "Moore"
+// console.log(child.hasOwnProperty('heritage')); // // повертає false
+// console.log(child.heritage); // // повертає "Irish"
+// console.log(parent.isPrototypeOf(child)); // // повертає true
 
+//5
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// // Change code above this line
+
+// console.log(ancestor.isPrototypeOf('parent')); //повертає true -
+// console.log(parent.isPrototypeOf('child')); //повертає true -
+// console.log(ancestor.hasOwnProperty('surname')); //повертає true
+// console.log(ancestor.surname); //повертає "Dawson"
+// console.log(parent.hasOwnProperty('surname')); //повертає true
+// console.log(parent.surname); //повертає "Moore"
+// console.log(child.hasOwnProperty('surname')); //повертає false
+// console.log(child.surname); //повертає "Moore"
+// console.log(ancestor.hasOwnProperty('heritage')); //повертає true
+// console.log(ancestor.heritage); //повертає "Irish"
+// console.log(parent.hasOwnProperty('heritage')); //повертає false
+// console.log(parent.heritage); //повертає "Irish"
+// console.log(child.hasOwnProperty('heritage')); //повертає false
+// console.log(child.heritage); //повертає "Irish"
+
+//6
+// class Car { };
+// console.log(Car);
+// const carCreate = new Car({});
+// console.log(carCreate);
+
+//7
+// class Car {
+//   // Change code below this line
+//   // constructor({ brand, model, price })
+//   constructor( brand, model, price ) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+// const audi = new Car('Audi', 'Q3', 36000);
+// // const audi = new Car({
+// //   brand: 'Audi',
+// //   model: 'Q3',
+// //   price: 36000,
+// // });
+
+// console.log(audi);
+
+//8
+// class Car {
+//   // Change code below this line
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
+//9
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+//10
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     for (const item of this.items) {
+//       if (item === itemToRemove) {
+//         this.items.splice(this.items.indexOf(item), 1);
+//       }
+//     }
+//   }
+//   }
+// // Change code above this line
+// const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+//11
+// class StringBuilder {
+//   constructor(initialValue) {
+//     this.value = initialValue;
+//   }
+//   getValue() {
+//     return this.value;
+//   }
+//   padStart(initialValue) {
+//     this.value = initialValue + this.value;
+//   }
+//   padEnd(initialValue) {
+//     this.value += initialValue;
+//   }
+//   padBoth(initialValue) {
+//     this.value = initialValue + this.value;
+//     this.value += initialValue;
+//   }
+// }
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+//12
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+//   // Change code above this line
+// }
+
+// const car1 = new Car({ brand: "Audi", model: "Q3", price: 36000 })
+// console.log(car1);
+// // утвориться об'єкт { model: "Q3", price: 36000 }
+
+// const car2 = new Car({ brand: "bmw", model: "X5", price: 58900 })
+// console.log(car2);
+// // утвориться об'єкт { model: "X5", price: 58900 }
+
+// const car3 = new Car({ brand: "Nissan", model: "Murano", price: 31700 })
+// console.log(car3);
+// // утвориться об'єкт { model: "Murano", price: 31700 }
+
+// console.log(car1.getBrand()); //повертає значення приватної властивості brand.
+// car2.changeBrand('Honda'); //змінює значення приватної властивості brand на "Honda"
+// console.log(car2);
+
+//13
+// class Storage {
+//   // Change code below this line
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+//14
+// class StringBuilder {
+//   // Change code below this line
+
+//   #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+//15
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   #model;
+//   #price;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+//16
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+//   #price;
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//     // this.#price = newPrice <= Car.MAX_PRICE ? this.#price : newPrice;
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+//   // Change code above this line
+// }
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+//17
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return 'Error! Price exceeds the maximum';
+//     } else {
+//       return 'Success! Price is within acceptable limits';
+//     }
+//   }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+//18
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
+// class Admin extends User {
+//   static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' };
+// }
+
+//19
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   accessLevel;
+//   // Change code below this line
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+//20
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+
+  constructor({ email, accessLevel, blacklistedEmails }) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = [];
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+
+  // Change code above this line
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
